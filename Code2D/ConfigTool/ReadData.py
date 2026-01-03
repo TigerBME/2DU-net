78a87bbcf829d239c3f418a6e57dda0478ec826c
TigerBME <13062706046@163.com>
2026-01-03 20:36:12 +0800

    增加了直接读取nifty并存为png的功能

--- Code2D/ConfigTool/ReadData.py
+++ Code2D/ConfigTool/ReadData.py
@@ -47,12 +47,66 @@ def nifti_to_bmp(nifti_file_path: str, bmp_file_folder: str, cutting_dimension: 
 
             # 创建图像并保存
             img = Image.fromarray(slice_data)
-            img_path = os.path.join(bmp_file_folder, f'slice_{slice_index}.bmp')
+            img_path = os.path.join(bmp_file_folder, f'slice_{slice_index:4d}.bmp')
             img.save(img_path)
 
             bmp_paths.append(img_path)
 
         return bmp_paths
+    except Exception as e:
+        print(f"转换过程中发生错误: {e}")
+        return []
+
+def nifti_to_png(nifti_file_path: str, png_file_folder: str, cutting_dimension: int) -> list:
+    '''
+    从nifti文件中读取数据，沿指定维度(1，2，3)切片
+    并保存为png文件。
+    :param nifti_file_path: nifti文件路径
+    :param png_file_folder: png文件保存路径
+    :param cutting_dimension: 切片维度
+    :return: 包含所有png文件地址的列表
+    '''
+    try:
+        # 加载NIfTI文件
+        nifti_image = nib.load(nifti_file_path)
+        nifti_data = nifti_image.get_fdata()
+
+        # 获取切片数量
+        if cutting_dimension == 0:
+            num_slices = nifti_data.shape[0]
+        elif cutting_dimension == 1:
+            num_slices = nifti_data.shape[1]
+        elif cutting_dimension == 2:
+            num_slices = nifti_data.shape[2]
+        else:
+            raise ValueError("切片维度应为0，1或2")
+        
+
+        # 确保保存文件夹存在
+        if not os.path.exists(png_file_folder):
+            os.makedirs(png_file_folder)
+
+        png_paths = []
+
+        # 遍历每个切片并保存为PNG文件
+        for slice_index in range(num_slices):
+            if cutting_dimension == 0:
+                slice_data = nifti_data[slice_index, :, :]
+            elif cutting_dimension == 1:
+                slice_data = nifti_data[:, slice_index, :]
+            elif cutting_dimension == 2:
+                slice_data = nifti_data[:, :, slice_index]
+
+            slice_data = slice_data.astype(np.uint8)
+
+            # 创建图像并保存
+            img = Image.fromarray(slice_data)
+            img_path = os.path.join(png_file_folder, f'slice_{slice_index:4d}.png')
+            img.save(img_path)
+
+            png_paths.append(img_path)
+
+        return png_paths
     except Exception as e:
         print(f"转换过程中发生错误: {e}")
         return []
